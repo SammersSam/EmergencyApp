1e83f7c3bc55910cc8ca1a2439ee331924201f9c
samlag <samlag@wp.pl>
2025-02-21 19:02:38 +0100

    Initial local commit

---
+++ src/test/java/com/example/emergencyapp/EmergencyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emergencyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
